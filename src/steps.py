92e2b7763c7727e894a7ff2f22268ae3b9596f29
vitornuness <vitor1nuness@gmail.com>
2025-04-16 22:20:36 -0300

    processamento

---
+++ src/steps.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Steps:
+
+    def process(self, df: pd.DataFrame):
+
+        return df
+    
+
+    def engineer(self, df: pd.DataFrame):
+
+        return df
+    
+    
+    def select(self, df: pd.DataFrame, is_predict=False):
+
+        return df
